23bcf832df08e17edbed928c3ce3748063f7e3ae
Anh Tran <anhtr077@gmail.com>
2024-02-17 16:27:32 -0800

    Updated files

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(cors());
 app.use(express.static(`${__dirname}/public`));
 
 //Routes
-app.get('/api/cat', (req, res) => res.send('Smelly cat'));
+app.get('/api/cat/', (req, res) => res.status(200).send('Smelly cat'));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
